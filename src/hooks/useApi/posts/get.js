64290f3410b39d307ba6b586a146ddfcaacdb21a
jjlmoya <juanjo1989@gmail.com>
2022-05-06 22:07:10 +0200

    Fix useApi

--- src/hooks/useApi/posts/get.js
+++ src/hooks/useApi/posts/get.js
@@ -2,7 +2,7 @@ import useAxios from '../useAxios'
 
 const useSampleGet = ({ url, method, body = null, headers = null }) => {
     const { response, loading, error } = useAxios({
-        method: 'pogetst',
+        method: 'post',
         url: '/posts',
         params: {
             test: true
